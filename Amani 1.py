8c890fb540011d1b1f99d686b418ef91e3aca9e3
Amani-k-cpu <khalifaamani39@gmail.com>
2023-07-25 12:20:35 +0400

    commit

---
+++ Amani 1.py
@@ -0,0 +1,18 @@
+from math import pi
+
+t_high = 5
+t_width = 4
+s_hight = 4
+s_width = 4
+half_c = 2
+
+
+
+t = (t_width * t_high) / 2
+s = s_hight * s_width
+c = pi * half_c ** 2
+
+print(t + (s - c))
+
+
+
